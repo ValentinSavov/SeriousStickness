bac350dbbef29daca7cbd904338733d9373cfa3e
ValentinSavov <valentin.savov.edge@gmail.com>
2017-03-28 14:58:20 +0300

    Test new file
    created in unity

---
+++ Assets/TestForGit.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestForGit : MonoBehaviour
+{
+    //here i just want to test the git commit stuff - 01
+}
+
+	
