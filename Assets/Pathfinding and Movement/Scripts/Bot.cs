e3b1c87746c7d20e1fbf68824ba2775919cf8fb4
ValentinSavov <valentin.savov.edge@gmail.com>
2017-03-31 20:49:06 +0300

    Simple AI (not GOAP) work

--- Assets/Pathfinding and Movement/Scripts/Bot.cs
+++ Assets/Pathfinding and Movement/Scripts/Bot.cs
@@ -42,9 +42,10 @@ public class Bot : Character
     void Start()
     {
         // this simulates control over Character class just like user inputs
-        BotInit(new bool[(int)KeyInput.Count], new bool[(int)KeyInput.Count]);
+        CharacterInit(new bool[(int)KeyInput.Count], new bool[(int)KeyInput.Count]);
+        BehaviorInit();
     }
-    void BotInit(bool[] inputs, bool[] prevInputs)
+    void CharacterInit(bool[] inputs, bool[] prevInputs)
     {
         mInputs = inputs;
         mPrevInputs = prevInputs;
@@ -189,15 +190,8 @@ public class Bot : Character
 
     void FixedUpdate()
     {
-        FindTarget();
         BotUpdate();
     }
-
-    void FindTarget()
-    {
-
-    }
-
 	void BotUpdate()
 	{
         mInputs[(int)KeyInput.GoRight] = false;
@@ -444,4 +438,141 @@ public class Bot : Character
         if (gameObject.activeInHierarchy)
 		    CharacterUpdate();
 	}
+
+
+#region States AI Stuff
+    private FSM stateMachine;
+    private FSM.FSMState idleState; // finds something to do
+    private FSM.FSMState moveToState; // moves to a target
+    private FSM.FSMState performActionState; // performs an action
+    void BehaviorInit()
+    {
+        //stateMachine = new FSM();
+        //createIdleState();
+        //createMoveToState();
+        //createPerformActionState();
+        //stateMachine.pushState(idleState);
+    }
+
+    void ResetFSM()
+    {
+        stateMachine = new FSM();
+        stateMachine.pushState(idleState);
+    }
+
+    private void createIdleState()
+    {
+        idleState = (fsm, gameObj) => {
+            // GOAP planning
+
+            // get the world state and the goal we want to plan for
+            //HashSet<KeyValuePair<string, object>> worldState = dataProvider.getWorldState();
+            //HashSet<KeyValuePair<string, object>> goal = dataProvider.createGoalState();
+
+            // Plan
+            //Queue<GoapAction> plan = planner.plan(gameObject, availableActions, worldState, goal);
+            //if (plan != null)
+            {
+                // we have a plan, hooray!
+                //currentActions = plan;
+                //dataProvider.planFound(goal, plan);
+
+                fsm.popState(); // move to PerformAction state
+                fsm.pushState(performActionState);
+
+            }
+            //else
+            {
+                // ugh, we couldn't get a plan
+                //Debug.Log("<color=orange>Failed Plan:</color>" + prettyPrint(goal));
+                //dataProvider.planFailed(goal);
+                fsm.popState(); // move back to IdleAction state
+                fsm.pushState(idleState);
+            }
+        };
+    }
+
+    private void createMoveToState()
+    {
+        moveToState = (fsm, gameObj) => {
+            // move the game object
+
+            //GoapAction action = currentActions.Peek();
+            //if (action.requiresInRange() && action.target == null)
+            {
+                Debug.Log("<color=red>Fatal error:</color> Action requires a target but has none. Planning failed. You did not assign the target in your Action.checkProceduralPrecondition()");
+                fsm.popState(); // move
+                fsm.popState(); // perform
+                fsm.pushState(idleState);
+                return;
+            }
+
+            // get the agent to go to action spot
+            //if (dataProvider.GoToAction(action))
+            {
+                fsm.popState();
+            }
+        };
+    }
+
+    private void createPerformActionState()
+    {
+        performActionState = (fsm, gameObj) => {
+            // perform the action
+
+            //if (!hasActionPlan())
+            {
+                // no actions to perform
+                Debug.Log("<color=red>Done actions</color>");
+                fsm.popState();
+                fsm.pushState(idleState);
+                //dataProvider.actionsFinished();
+                return;
+            }
+
+            //GoapAction action = currentActions.Peek();
+            //if (action.isDone())
+            {
+                // the action is done. Remove it so we can perform the next one
+                //currentActions.Dequeue();
+            }
+
+            //if (hasActionPlan())
+            {
+                // perform the next action
+                //action = currentActions.Peek();
+                //bool inRange = action.requiresInRange() ? action.isInRange() : true;
+
+                //if (inRange)
+                {
+                    // we are in range, so perform the action
+                    //bool success = action.perform(gameObj);
+
+                    //if (!success)
+                    {
+                        // action failed, we need to plan again
+                        fsm.popState();
+                        fsm.pushState(idleState);
+                        //dataProvider.planAborted(action);
+                    }
+                }
+                //else
+                {
+                    // we need to move there first
+                    // push moveTo state
+                    fsm.pushState(moveToState);
+                }
+
+            }
+            //else
+            {
+                // no actions left, move to Plan state
+                fsm.popState();
+                fsm.pushState(idleState);
+                //dataProvider.actionsFinished();
+            }
+
+        };
+    }
+    #endregion
 }
